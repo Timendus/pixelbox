4229f811878dc47485a3f39d543c062a068ea0eb
Tim Franssen <mail@timendus.com>
2026-02-11 17:16:19 +0100

    Add some documentation for future me

--- client/js/databind.js
+++ client/js/databind.js
@@ -1,3 +1,172 @@
+/**
+ * This class allows you to bind data to DOM elements, either one way or
+ * bi-directional. This enables you to keep your domain model in sync with
+ * what's shown in the browser.
+ *
+ * Bind part of the DOM to a data object:
+ *
+ * ```js
+ * const dataProxy = new Databind("#app", {
+ *   user: {
+ *     name: "Alice",
+ *     visible: true,
+ *     role: "admin",
+ *   },
+ *   clickCounter: 0,
+ * });
+ * ```
+ *
+ * Then you can use the following attributes in the DOM to bind to that data:
+ *
+ *  - `data-bind`: Two-way binding. The value of the attribute is a path in the
+ *    data object. The value of the DOM element will be kept in sync with the
+ *    value in the data object, and changes to the DOM element will be written
+ *    back to the data object.
+ *  - `data-read`: One-way binding. The value of the attribute is a path in the
+ *    data object. Changes to the data object at the given path will get synced
+ *    to the DOM element.
+ *  - `data-write`: One-way binding. The value of the attribute is a path in the
+ *    data object. Changes to the DOM element will be written to the data object
+ *    at that path.
+ *  - `data-click`: The value of the attribute is an assignment expression. When
+ *    the element is clicked, the expression will be evaluated and the result
+ *    will be stored in the data object. For example,
+ *    `data-click="counter=counter+1"` will increment the `counter` property in
+ *    the data object when the element is clicked.
+ *  - `data-active-if`: The value of the attribute is a path or an expression
+ *    (see below). If the expression evaluates to a truthy value, the class
+ *    specified in the `class` option (default: "active") will be added to the
+ *    element. Otherwise, it will be removed.
+ *
+ * For example:
+ *
+ * ```html
+ * <div id="app" data-active-if="user.visible">
+ *   <input type="text" data-bind="user.name">
+ *   <p data-read="user.role"></p>
+ *   <button data-click="user.role=admin">Admin</button>
+ *   <button data-click="user.role=contrib">Contributor</button>
+ *   <p data-read="clickCounter"></p>
+ *   <button id="counter">Click me</button>
+ * </div>
+ * ```
+ *
+ * The constructor returns a proxy object which you can use to read and write to
+ * the data object. Changes you make through this proxy will be reflected in the
+ * DOM:
+ *
+ * ```js
+ * dataProxy.data.user.visible = false;   // This will hide the entire div with id "app"
+ * console.log(dataProxy.data.user.name); // This will log "Alice"
+ *
+ * // Listen for clicks on the button, and update the counter shown
+ * document.getElementById("counter").addEventListener("click", () =>
+ *   dataProxy.data.clickCounter++);
+ * ```
+ *
+ * You can also listen for changes to the data object by adding event listeners
+ * to the returned object:
+ *
+ * ```js
+ * dataProxy.addEventListener("change", (path, oldValue, newValue) => {
+ *   console.log(`Change at ${path} from ${oldValue} to ${newValue}`);
+ * });
+ * ```
+ *
+ * ## Expressions
+ *
+ * The values of the `data-active-if` and `data-click` attributes can be more
+ * than just paths in the data object. They allow for simple expressions that
+ * include comparisons and assignments. For example, `data-active-if="user.age
+ * >= 18"` will add the active class to the element if the user's age is greater
+ * than or equal to 18. `data-active-if="user.name==Alice"` will add the active
+ * class if the user's name is Alice. The supported comparisons are `==`, `!=`,
+ * `>`, `<`, `>=`, and `<=`.
+ *
+ * The `data-click` attribute supports assignment expressions. For example,
+ * `data-click="user.name=John"` will set the user's name to John when the
+ * element is clicked.
+ *
+ * If you want to do something more complicated than what these simple
+ * expressions allow, just write proper event listeners that manipulate the data
+ * object, like in the examples above.
+ *
+ * ## Loops
+ *
+ * You can also use the `data-loop` attribute to repeat a section of the DOM for
+ * each element in an array in the data object. The value of the `data-loop`
+ * attribute should be an assignment expression where the assignee is a variable
+ * name and the assigned value is a path to an array in the data object. For
+ * example, `data-loop="item=items"` will repeat the section of the DOM for each
+ * element in the `items` array in the data object, and within that section you
+ * can use `item` to refer to the current element. The original contents of the
+ * element with the `data-loop` attribute will be used as a template for each
+ * repeated section.
+ *
+ * For example:
+ *
+ * ```html
+ * <ul data-loop="item=items">
+ *   <li data-read="item"></li>
+ * </ul>
+ * ```
+ *
+ * ## Options
+ *
+ * The DataBind constructor takes an optional third argument with the following
+ * options:
+ *
+ * - `class`: The class to toggle for `data-active-if` expressions (default:
+ *   "active")
+ * - `stopEvents`: Whether to call `preventDefault` and `stopPropagation` on
+ *   events after handling them (default: true)
+ * - `immediate`: Whether to listen for `input` events instead of `change`
+ *   events for form elements (default: false). If true, changes to form
+ *   elements will be written to the data object immediately as the user types,
+ *   instead of only when they blur the element.
+ * - `customConversions`: An array of custom conversion objects. Each object
+ *   should have a `selector` property, and optionally `toDom` and `fromDom`
+ *   functions. If an element matches the selector, the corresponding conversion
+ *   functions will be used to convert values between the data object and the
+ *   DOM, instead of the default conversions. The `toDom` function takes three
+ *   arguments: the path that changed in the data object, the new value at that
+ *   path, and the DOM element to update. The `fromDom` function takes two
+ *   arguments: the path to write to in the data object, and the DOM element to
+ *   read from. It should return the value to write to the data object.
+ *
+ * For example:
+ *
+ * ```js
+ * const dataProxy = new Databind("#app", {
+ *   date: new Date(),
+ * }, {
+ *   class: "enabled",
+ *   immediate: true,
+ *   customConversions: [
+ *     {
+ *       selector: "input[type=date]",
+ *       toDom: (path, value, element) => element.value = value.toISOString().substring(0, 10),
+ *       fromDom: (path, element) => new Date(element.value),
+ *     },
+ *   ],
+ * });
+ * ```
+ *
+ * You can also override the attribute names for the bindings by passing
+ * different values in the options object. For example, if you want to use
+ * `data-model` instead of `data-bind` and `data-is-active` instead of
+ * `data-active-if` for some reason, you can do:
+ *
+ * ```js
+ * const dataProxy = new Databind("#app", {...}, {
+ *   bind: "data-model",
+ *   activeIf: "data-is-active"
+ * });
+ * ```
+ *
+ * @module
+ */
+
 import { DataProxy, toValue } from "./dataproxy.js";
 
 export default class Databind {
